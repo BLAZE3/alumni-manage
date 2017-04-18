422b25c310fc95d6d514925ead3d70dd9ec395fb
LiuLei <tobyond@126.com>
2017-04-18 16:33:09 +0800

    邮件发送,登录页面

--- alumni-manage/src/main/java/cn/blaze/controller/MailController.java
+++ alumni-manage/src/main/java/cn/blaze/controller/MailController.java
@@ -1,18 +1,16 @@
 package cn.blaze.controller;
 
 import java.util.Properties;
-import java.util.Random;
 
 import javax.mail.Address;
 import javax.mail.Authenticator;
-import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
+import javax.mail.internet.MimeUtility;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -23,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MailController {
 	
 	@RequestMapping("sendMail")
-	public void sendMail(HttpServletRequest request){
+	public String sendMail(HttpServletRequest request){
 		
 		try {
 			//第一步：声明properties放信息对象
@@ -37,10 +35,12 @@ public class MailController {
 				//返回用户名和密码的对象
 				public PasswordAuthentication getPasswordAuthentication()
 				{
-					PasswordAuthentication pa=new PasswordAuthentication("tobyond", "yxj911103+");
+					PasswordAuthentication pa=new PasswordAuthentication("tobyond", "Liu982195");
 					return pa;
 				}
 			};
+			//获得邮件对象
+//			Session session=Session.getInstance(props, authenticator); // 发送不是使用同一个邮箱时使用
 			//获得邮件对象
 			Session session=Session.getDefaultInstance(prop, auth);
 			//设置session的调试模式
@@ -53,13 +53,13 @@ public class MailController {
 			//第五步：设置收件人
 			mm.setRecipient(RecipientType.TO, new InternetAddress("840172843@qq.com"));
 			//第六步：设置主题
-			mm.setSubject("测试邮箱主题");
-			
+			mm.setSubject( MimeUtility.encodeText( "邮件标题-测试发送","gb2312","B"));
 			mm.setContent("测试内容：这是邮箱的测试内容。", "text/plain;charset=utf-8");
 			//第七步： 发送邮件
 			Transport.send(mm);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return "success";
 	}
 }
